c3f2b217fcd2a3dfe760c037e2f3944b2f895976
leon-dream1~ <nahidulislam019045@gmail.com>
2024-04-27 23:16:00 +0600

    banner section added with images

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,9 +1,20 @@
 const Banner = () => {
   return (
     <div className="carousel w-full">
-      <div id="slide1" className="carousel-item relative w-full">
-        <div className="bg-banner1 ">
-            <h1>Explore</h1>
+      <div
+        id="slide1"
+        className="carousel-item relative w-full  bg-banner1 bg-no-repeat bg-cover h-[862px]"
+      >
+        <div className="w-[60%] mx-auto flex justify-center items-center">
+          <div className="text-center">
+            <p className="text-[30px] text-black font-serif text-center font-medium">
+              Lets travel the world with us
+            </p>
+            <h1 className="text-[60px] text-white font-merriweather font-extrabold">
+              Explore The World With{" "}
+              <span className="text-amber-500">TRAVOL</span>
+            </h1>
+          </div>
         </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide5" className="btn btn-circle">
@@ -14,8 +25,20 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full">
-        <div className="bg-banner1 "></div>
+      <div
+        id="slide2"
+        className="carousel-item relative w-full bg-banner2 bg-no-repeat bg-cover h-[862px]"
+      >
+        <div className="w-[60%] mx-auto flex justify-center items-center">
+          <div className="text-center">
+            <p className="text-[30px] text-black font-serif text-center font-medium">
+              Lets travel the world with us
+            </p>
+            <h1 className="text-[60px] text-blue-600 font-merriweather font-extrabold">
+              Explore Rome With <span className="text-amber-500">TRAVOL</span>
+            </h1>
+          </div>
+        </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide1" className="btn btn-circle">
             ❮
@@ -25,8 +48,21 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide3" className="carousel-item relative w-full">
-        <div className="bg-banner1 "></div>
+      <div
+        id="slide3"
+        className="carousel-item relative w-full bg-banner6 bg-no-repeat bg-cover h-[862px]"
+      >
+        <div className="w-[60%] mx-auto flex justify-center items-center">
+          <div className="text-center">
+            <p className="text-[30px] text-black font-serif text-center font-medium">
+              Lets travel the world with us
+            </p>
+            <h1 className="text-[60px] text-white font-merriweather font-extrabold">
+              Discover The World With{" "}
+              <span className="text-amber-500">Our Guide</span>
+            </h1>
+          </div>
+        </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide2" className="btn btn-circle">
             ❮
@@ -36,8 +72,21 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide4" className="carousel-item relative w-full">
-        <div className="bg-banner1 "></div>
+      <div
+        id="slide4"
+        className="carousel-item relative w-full bg-banner7 bg-no-repeat bg-cover h-[862px]"
+      >
+        <div className="w-[60%] mx-auto flex justify-center items-center">
+          <div className="text-center">
+            <p className="text-[30px] text-black font-serif text-center font-medium">
+              Lets travel the world with us
+            </p>
+            <h1 className="text-[60px] text-white font-merriweather font-extrabold">
+              Discover Your Wanderlust Where Will Your
+              <span className="text-amber-500"> Next Journey !</span>
+            </h1>
+          </div>
+        </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide3" className="btn btn-circle">
             ❮
@@ -47,8 +96,21 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide5" className="carousel-item relative w-full">
-        <div className="bg-banner1"></div>
+      <div
+        id="slide5"
+        className="carousel-item relative w-full bg-banner5 bg-no-repeat bg-cover h-[862px]"
+      >
+        <div className="w-[60%] mx-auto flex justify-center items-center">
+          <div className="text-center">
+            <p className="text-[30px] text-black font-serif text-center font-medium">
+              Lets travel the world with us
+            </p>
+            <h1 className="text-[60px] text-white font-merriweather font-extrabold">
+              Where Will Your Imagination Take You
+              <span className="text-amber-500"> Today?</span>
+            </h1>
+          </div>
+        </div>
         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
           <a href="#slide4" className="btn btn-circle">
             ❮
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'raleway': '"Raleway", sans-serif', 
-        'montserrat': '"Montserrat", sans-serif', 
-        'merriweather': '"Merriweather", serif', 
-        'inter': '"Inter", sans-serif',  
+        raleway: '"Raleway", sans-serif',
+        montserrat: '"Montserrat", sans-serif',
+        merriweather: '"Merriweather", serif',
+        inter: '"Inter", sans-serif',
       },
       backgroundImage: {
-        "banner1" : "url('/public/banner1.jpg')",
-        "banner2": "url('/public/banner2.jpg')",
-        "banner3" : "url('/public/maldives-island_1203-2584.jpg')",
-        "banner4" : "url('/public/banner4.jpg')",
-        "banner5" : "url('/public/banner5.jpg')"
-      }
+        banner1: "url('/public/banner1.jpg')",
+        banner2: "url('/public/banner2.jpg')",
+        banner3: "url('/public/banner3.jpg')",
+        // "banner3" : "url('/public/maldives-island_1203-2584.jpg')",
+        banner4: "url('/public/banner4.jpg')",
+        banner5: "url('/public/banner5.jpg')",
+        banner6: "url('/public/banner6.jpg')",
+        banner7: "url('/public/banner7.jpg')",
+      },
     },
   },
   plugins: [require("daisyui")],
-}
-
+};
